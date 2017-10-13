7d9b2a0c519228a7fdaf4a598f69362eebbd502d
Zhangfan Dong <dong.zhangfan@gmail.com>
2017-10-13 12:52:01 -0700

    Fix typo in functional test

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -107,7 +107,7 @@ class NewVisitorTest(LiveServerTestCase):
         # He is less interesting than Edith...
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Buy milk')
-        inputbox.sned_keys(Keys.ENTER)
+        inputbox.send_keys(Keys.ENTER)
         self.wait_for_row_in_list_table('1: Buy milk')
 
         # Francis gets his own unique URL
